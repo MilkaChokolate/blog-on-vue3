517bbf1e7c7cd3e6255585971bcc94eddacfce18
MilkaChokolate <87569186+MilkaChokolate@users.noreply.github.com>
2022-06-10 15:30:23 +0300

    Update AboutPage.vue

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>First project on VUE-3</h1>
+    <h1>Blog on VUE-3</h1>
     <h2>POSTS - mapping done with props</h2>
     <h2>STORE - mapping done with store</h2>
     <h2>COMPOSITION - mapping done with composition</h2>
